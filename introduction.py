33a7c966629fcfa4e88468022aab6ec6826ce202
Chumbaandrew <sharonafa36@gmail.com>
2023-08-17 09:19:21 +0300

    fourth commit

---
+++ introduction.py
@@ -0,0 +1,7 @@
+print("welcome to python")
+print("my name is Andrew")
+print(7+8)
+print("8+9")
+
+
+
---
+++ operators.py
@@ -0,0 +1,18 @@
+x=9
+y=78
+a=10
+b=9
+#arithmetic operators
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+# comparison operators
+print("is equal to", x==y)
+print("is not equal to", x!=y)
+print("greater than", x>y)
+print("greater than", x<y)
+#logiacl operators
+print("is equal to",x==y and a==b)
+print("is equal to",x==y or a==b)
